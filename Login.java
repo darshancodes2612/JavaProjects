bf6062b4c3911f05a1a51cef07bdef0a3d8eae28
Darshan <joshi.darshan1826@gmail.com>
2023-06-08 12:47:50 +0530

    Initial commit

---
+++ Login.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Login {
+    public static void main(String args[]){
+        Frame obj = new Frame();
+        obj.setVisible(true);
+        obj.setTitle("Login Form");
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.setResizable(true);
+        obj.setBounds(10, 10, 370, 450);
+    }
+}
